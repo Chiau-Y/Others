f3511eee975fd16d1d42c5791be13c3f8252b304
Chiauyuan_ICLab <l29522523@gmail.com>
2021-06-01 12:25:04 +0800

    20210601

--- Practice/BinarySearch.c
+++ Practice/BinarySearch.c
@@ -4,12 +4,12 @@ int BinarySearchRecursion(int x, int begin, int end, int *ptr){
     
     int answer;
     
-    if (begin > end) return -1;
+    if (begin > end) return -1;  // FALSE, 0
     
     int mid = (end + begin)/2;
     if (ptr[mid] < x) answer = BinarySearchRecursion(x, mid+1, end, ptr);
     else if (ptr[mid] > x) answer = BinarySearchRecursion(x, begin, mid-1, ptr);
-    else return mid;
+    else return mid;    // TRUE, 1
     
     return answer;
     
@@ -23,10 +23,10 @@ int BinarySearch(int x, int begin, int end, int *ptr){
         mid = (end + begin)/2;
         if (ptr[mid] < x) begin = mid +1;
         else if (ptr[mid] > x) end = mid-1;  
-        else return mid;
+        else return mid;    // TRUE, 1
     }
     
-    return begin;
+    return -1;  // FLASE, 0
 }
 
 int main()
@@ -42,6 +42,8 @@ int main()
     printf("%d\n",ans);  
     ans = BinarySearchRecursion(35, 0, sizeof(arr)/sizeof(arr[0])-1, &arr[0]);
     printf("%d\n",ans); 
+    ans = BinarySearchRecursion(700, 0, sizeof(arr)/sizeof(arr[0])-1, &arr[0]);
+    printf("%d\n",ans);
     
     // While Loop
     ans = BinarySearch(2, 0, sizeof(arr)/sizeof(arr[0])-1, &arr[0]);
@@ -49,6 +51,8 @@ int main()
     ans = BinarySearch(100, 0, sizeof(arr)/sizeof(arr[0])-1, &arr[0]);
     printf("%d\n",ans);  
     ans = BinarySearch(35, 0, sizeof(arr)/sizeof(arr[0])-1, &arr[0]);
+    printf("%d\n",ans);
+    ans = BinarySearch(700, 0, sizeof(arr)/sizeof(arr[0])-1, &arr[0]);
     printf("%d\n",ans);
     
     return 0;
